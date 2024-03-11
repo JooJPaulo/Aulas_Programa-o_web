42b0ef314f40d482871f4fb7c448c132cb5fe34f
João Paulo Barros <108131013+JooJPaulo@users.noreply.github.com>
2024-03-11 19:25:38 -0300

    Add files via upload

---
+++ Aula00/dados.js
@@ -0,0 +1,4 @@
+var nome = prompt("Qual é o seu nome?");
+var sobrenome = prompt("Qual é o seu sobrenome?");
+console.log("Olá, " + nome + " " + sobrenome + "!");
+document.write("Olá, " + nome + " " + sobrenome + "!");
